6c02cbc5486a17255a84045cfc7aa9c226d1baf9
IT26102364 <IT26102364@my.sliit.lk>
2026-09-13 23:14:07 +0530

    IT26102364

---
+++ Q2/IT26102364Lab6Q2B.java
@@ -0,0 +1,26 @@
+public class  IT26102364Lab6Q2B{
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int[] numbers = new int[10];
+
+        System.out.println("Please enter 10 numbers:");
+
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Enter number " + (i + 1)  + ": ");
+            numbers[i] = input.nextInt();
+        }
+
+
+        System.out.println("\nThe numbers you entered are:");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print(numbers[i] + " ");
+        }
+
+        input.close();
+    }
+}
